08f068930f1f7990380fcaf7686622170ac557aa
ChenHaoYan1234 <chanhaoyan@126.com>
2025-11-02 09:11:26 +0800

    Add VSCode debug config and integrate echarts dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: { enabled: true },
-    modules: ['@nuxt/eslint', '@nuxt/ui']
+    modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/icon', 'nuxt-echarts'],
+    pages: true,
+    devServer: {
+        port: 8080
+    }
 })
